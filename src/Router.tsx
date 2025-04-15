98116e4e60bb16b37cbb4696f4cb0a1d0787c530
rodom_macbook <enjoying1018@naver.com>
2025-04-16 00:18:45 +0900

    feat: 홈만들기(초안)

--- src/Router.tsx
+++ src/Router.tsx
@@ -1,11 +1,13 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import SpringProblemPage from "./pages/SpringProblem/SpringProblemPage";
 import SubmitListPage from "./pages/SubmitList/SubmitListPage";
+import { HomePage } from "./pages/Home/HomePage";
 
 const Router = () => {
     return(
         <BrowserRouter>
             <Routes>
+                <Route path="" element={<HomePage></HomePage>}></Route>
                 <Route path="/problem/:problemId" element={<SpringProblemPage/>}></Route>
                 <Route path="/submit/:problemId" element={<SubmitListPage></SubmitListPage>}></Route>
             </Routes>
